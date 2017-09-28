62d39303e2bbeb41869ce083b66f94b2c71685e3
Aaron Samuel <aasamuelj@gmail.com>
2017-09-28 21:03:58 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,7 +5,7 @@ class School
     @roster = {}
   end
   def add_student(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name
+    @roster[grade] ||= []
+    @roster[grade] == name ||= << name
   end
 end
